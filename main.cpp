d2303f2172b32f17e09139a06a01585eed8ca595
26730019-Hieu <26730019@ms.uit.edu.vn>
2026-09-08 23:07:21 +0700

    Refactor Ve function to restore drawing logic

--- main.cpp
+++ main.cpp
@@ -17,12 +17,6 @@ public:
         A[1].x = 11; A[1].y = 10;
         A[2].x = 12; A[2].y = 10;
     }
-    void Ve(){
-        for (int i = 0; i < DoDai; i++){
-            gotoxy(A[i].x,A[i].y);
-            cout<<"X";
-        }
-    }
     void DiChuyen(int Huong){
         for (int i = DoDai-1; i>0;i--)
             A[i] = A[i-1];
@@ -31,6 +25,12 @@ public:
         if (Huong==2) A[0].x = A[0].x - 1;
         if (Huong==3) A[0].y = A[0].y - 1;
 
+    }
+    void Ve(){
+        for (int i = 0; i < DoDai; i++){
+            gotoxy(A[i].x,A[i].y);
+            cout<<"X";
+        }
     }
 };
 
